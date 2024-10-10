157423ec58726e0c62377ff3253c22d4a5d5c636
ShadowLp174 <samuel.moravec@yahoo.com>
2024-10-11 01:37:26 +0200

    improved handling of sales
    The original price is now converted and displayed too

--- arx-converter-userscript.user.js
+++ arx-converter-userscript.user.js
@@ -4,7 +4,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.elitedangerous.com/store/*
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @author      ShadowLp174
 // @description 4.5.2024, 09:51:01
 // ==/UserScript==
@@ -59,7 +59,7 @@ const currency = currencies[useCurrency];
 const convertArx = (arx) => {
   // NOTE: This conversion algorithm only works with currencies that are based on hundreds. For example: 100 cents = 1 euro
 
-  // account for sales:
+  // account for sales: (theoretically obsolete now, but I'll keep it in just to be safe)
   arx = arx.split(" ")
   arx = arx[arx.length - 1] // select last element to display correct price if a sale is displayed
 
@@ -85,15 +85,30 @@ const convertArx = (arx) => {
 window.addEventListener("load", () => {
   var tags = document.body.getElementsByClassName("o-price")
 
-  const priceElems = [];
   for (let i = 0; i < tags.length; i++) {
-    priceElems.push(tags[i].children[0])
+    let arx = tags[i].children[0].innerText;
+    tags[i].children[0].remove();
+    arx = arx.split(" ")
+
+    tags[i].children[0].remove(); // remove arx symbol
+
+    // in case this is an item on sale, the original and the discounted price are computed and displayed
+    arx.forEach((a, j) => {
+      const price = (j === 0 && arx.length > 1) ? document.createElement("strike") : document.createElement("span")
+      if (j === 0 && arx.length > 1) price.style = "text-decoration: line-through; opacity: .4";
+      price.style.color = "#000";
+
+      price.innerText = convertArx(a)
+      tags[i].appendChild(price);
+      if (j !== 0) {
+        price.style.marginLeft = "3px"; // the gap a space would do
+      }
+    });
     const currencySymbol = document.createElement("span")
     currencySymbol.innerText = currency;
     currencySymbol.style = "margin-left: 4px";
-    tags[i].children[1].remove();
     tags[i].appendChild(currencySymbol)
   }
 
-  priceElems.map(e => e.innerText = convertArx(e.innerText))
+  //priceElems.map(e => e.innerText = convertArx(e.innerText))
 });
